d24c416c7b05916c9fc12c21e74dbe7da886edfc
Amritpal Singh <singhamritpal49@gmail.com>
2019-03-29 03:35:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,3 +18,19 @@ var holiday = "Mother's Day"
 function holidayCountdown(holiday,days) {
   return `It's ${days} days until ${holiday}!`
 }
+
+// function happyHolidays() {
+//   return "Happy holidays!"
+// }
+//
+// function happyHolidaysTo(name) {
+//   return `Happy holidays, ${name}!`
+// }
+//
+// function happyHolidayTo(holiday, name) {
+//   return `Happy ${holiday}, ${name}!`
+// }
+// 
+// function holidayCountdown(holiday, days) {
+//   return `It's ${days} days until ${holiday}!`
+// }
